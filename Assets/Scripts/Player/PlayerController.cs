e1b00b2eceb4b7e0941b83c9ca026dd488ee39e9
Thuan Vo <thuanvo09@hotmail.com>
2024-09-22 20:10:21 -0400

    Added simple splitting animation

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,16 +4,29 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            print("Split it");
+            animator.SetBool("Split", true);
+        }
+
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Simple Splitting") && 
+            animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f)
+        {
+            animator.SetBool("Split", false);
+        }
     }
 
 }
