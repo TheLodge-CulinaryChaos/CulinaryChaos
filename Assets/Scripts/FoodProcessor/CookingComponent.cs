e45481f752c89bb7e6c7622c05205e62c596ac0a
Thuan Vo <thuanvo09@hotmail.com>
2024-10-19 17:56:24 -0400

    reorganized level 3 and ensured each ingredient contains its properties

---
+++ Assets/Scripts/FoodProcessor/CookingComponent.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CookingComponent : MonoBehaviour
+{
+
+    public GameObject ingredient;
+    public GameObject pot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
