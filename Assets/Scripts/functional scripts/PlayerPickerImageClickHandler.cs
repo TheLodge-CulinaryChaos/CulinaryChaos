896da20664be4f65daceb1ef84a6bbee92c74a45
Thuan Vo <thuanvo09@hotmail.com>
2024-11-27 00:12:03 -0500

    update version, change rotate speed

--- Assets/Scripts/functional scripts/PlayerPickerImageClickHandler.cs
+++ Assets/Scripts/functional scripts/PlayerPickerImageClickHandler.cs
@@ -7,19 +7,13 @@ public class PlayerPickerImageClickHandler : MonoBehaviour
     public GameObject player;
 
     // player rotate around its Y axis
-    private float rotateSpeed = 50f;
+    private float rotateSpeed = 80f;
     public Enums playerType;
     public GameObject indicator;
 
     void Awake()
     {
         indicator.SetActive(false);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        player.transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime);
     }
 
     void LateUpdate()
